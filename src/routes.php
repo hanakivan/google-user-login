dd9a5fa4b9a73ef809a4adf28254b30b8c9bfd38
Ivan hanák <hanakivan@gmail.com>
2021-12-23 20:34:31 +0100

    Added middlewares so as the user stays logged in

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('google/oauth', [\hanakivan\GoogleUserLogin\GoogleController::class, "oauth"])->name("hanakivan.google.oauth");
+Route::get('google/oauth', [\hanakivan\GoogleUserLogin\GoogleController::class, "oauth"])
+    ->middleware([
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+    ])
+    ->name("hanakivan.google.oauth");
